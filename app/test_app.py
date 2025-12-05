25503c2fc77d47614b111b19de01d76e12c02642
kachmazov <kachmazovzaur123@gmail.com>
2025-12-05 23:57:03 +0000

    test

--- app/test_app.py
+++ app/test_app.py
@@ -198,7 +198,7 @@ def test_always_successful():
 def test_can_be_made_to_fail():
     """Тест который можно заставить упасть для демонстрации."""
     # Для нормальной работы:
-    should_pass = True
+    should_pass = False
     
     # Для демонстрации неудачного теста в лабораторной:
     # should_pass = False
